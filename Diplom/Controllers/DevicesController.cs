e2f8e4127c3fb89925d80a36a0781a3077eeb5a1
monah <monah_a_a@outlook.com>
2023-04-03 21:39:10 +0500

    Update all CRUD operation for Device model

--- Diplom/Controllers/DevicesController.cs
+++ Diplom/Controllers/DevicesController.cs
@@ -166,40 +166,69 @@ namespace Diplom.Controllers
         }
 
         // GET: Devices/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
         {
             if (id == null || _context.Device == null)
             {
                 return NotFound();
             }
 
-            var device = await _context.Device
+            var device = await _context.Device.AsNoTracking()
                 .FirstOrDefaultAsync(m => m.ID == id);
             if (device == null)
             {
                 return NotFound();
             }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewData["ErrorMessage"] =
+                    "Delete failed. Try again, and if the problem persists " +
+                    "see your system administrator.";
+            }
 
             return View(device);
         }
 
-        // POST: Devices/Delete/5
+        /* // POST: Devices/Delete/5
+         [HttpPost, ActionName("Delete")]
+         [ValidateAntiForgeryToken]
+         public async Task<IActionResult> DeleteConfirmed(int id)
+         {
+             if (_context.Device == null)
+             {
+                 return Problem("Entity set 'DiplomContext.Device'  is null.");
+             }
+             var device = await _context.Device.FindAsync(id);
+             if (device != null)
+             {
+                 _context.Device.Remove(device);
+             }
+
+             await _context.SaveChangesAsync();
+             return RedirectToAction(nameof(Index));
+         }*/
+
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Device == null)
+            var student = await _context.Device.FindAsync(id);
+            if (student == null)
             {
-                return Problem("Entity set 'DiplomContext.Device'  is null.");
+                return RedirectToAction(nameof(Index));
             }
-            var device = await _context.Device.FindAsync(id);
-            if (device != null)
+
+            try
             {
-                _context.Device.Remove(device);
+                _context.Device.Remove(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
             }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.)
+                return RedirectToAction(nameof(Delete), new { id = id, saveChangesError = true });
+            }
         }
 
         private bool DeviceExists(int id)
