dc22624790f2ea69a29e2080ac3e83559f3c263c
monah <monah_a_a@outlook.com>
2023-05-30 14:30:19 +0500

    Работа над инструкциями часть 1

---
+++ Diplom/wwwroot/js/summernoteTextEditor.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('#summernote').summernote({
+        lang: 'ru-RU',
+        height: 300,
+        minHeight: 200,
+
+        focus: true,
+        placeholder: '¬ведите данные',
+        toolbar: [
+            //[groupname,[list buttons]]
+            ['insert', ['picture', 'link', 'video', 'table']],
+            ['style', ['bold', 'italic', 'underline']],
+            ['font', ['strikethrough', 'superscript', 'subscript']],
+            ['fontsize', ['fontsize', 'fontname']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph', 'style']],
+            ['height', ['height', 'codeview']],
+
+        ],
+        fontNames: ['Arial', 'Times New Roman', 'Verdana'],
+        disableDragAndDrop: true,
+    });
+});
